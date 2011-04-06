d465ba88ba3ba5be09faef8480f0656f0f578683
mars.ninja <mars.ninja@8965d2bd-1284-5c8b-b627-98c280962c04>
2011-04-06 04:55:19 +0000

    I think this works, gonna pluck tomorrow

--- trunk/tools/ieee-bib-napper.py
+++ trunk/tools/ieee-bib-napper.py
@@ -8,19 +8,21 @@ os.system("cat *.html >> snag.clump; rm *.html");
 
 final=[];
 puns=[];
+jpuns=[];
 f=open("snag.clump",'r').readlines();
 for i in f:
   if(len(i.split("id=\"asf_pun\" name=\"asf_pun\" value=\""))>1):
-    puns.append(i.split("id=\"asf_pun\" name=\"asf_pun\" value=\"")[1].split("\"/>")[0]);
+    value = i.split("id=\"asf_pun\" name=\"asf_pun\" value=\"")[1].split("\"/>")[0];
+    if(value!="null"):
+      puns.append(i.split("id=\"asf_pun\" name=\"asf_pun\" value=\"")[1].split("\"/>")[0]);
   if(len(i.split("Volume:"))>1):
     if(len(i.split("Issue:"))>1):
       getit=i.split("\"]");
       for j in getit:
         j=j.split("\"");
-        j=j.pop();
-        print j;
+        if(len(j)>1):
+          jpuns.append(j.pop());
 
-exit(1);
 opener = urllib2.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
@@ -32,6 +34,14 @@ for i in puns:
     f = opener.open(site); s=f.read(); f.close(); 
     if(len(s.split("name=\"asf_pn\" value=\"null\""))>1): break;
     ids.write(s);
+for i in jpuns:
+  for j in range(150):
+    site="http://ieeexplore.ieee.org/xpl/tocresult.jsp?asf_arn=null&asf_iid="+i+"&asf_pun=null&asf_in=null&asf_rpp=null&asf_iv=null&asf_sp=null&asf_pn="+str(j+1);
+    print "plucking",i,"page",j+1;
+    f = opener.open(site); s=f.read(); f.close(); 
+    if(len(s.split("<strong>No articles found</strong>"))>1): break;
+    ids.write(s);
+  break;  
 
 
 ars=[];
